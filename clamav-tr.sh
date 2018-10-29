e728be19d7891ae38b15864354977e586c6e425a
coldnfire <laboserver@gmail.com>
2018-10-29 20:51:29 +0100

    Syntax correction

--- clamav-tr.sh
+++ clamav-tr.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# Maintainer : coldnfire, laboserver@gmail.com
 # Script "ClamAV real time", by coldnfire
-# Dependance: clamav-daemon 
+# Dependency : fswatch
  
 DOSSIER=$HOME
 user=echo $USER
 QUARANTAINE=/var/jail/
 LOG=/Users/jv/clamav-tr/clamav-tr/jail.log
+email=$email
 
 /usr/local/sbin/clamd
  
@@ -17,8 +19,8 @@ fswatch -l 1 $DOSSIER |
 while read FICHIER; do
 	clamdscan -m -v --fdpass "$FICHIER" --move=$QUARANTAINE
         if [ "$?" == "1" ]; then
-		echo -e "\033[31mMalware found!!!\033[00m" "File '$FICHIER' file has been mooved in jail !"
-		echo -e "Malware found" "File '$FICHIER' has been mooved in jail" | mail -s "Malware Found" laboserver@gmail.com
+		echo -e "\033[31mMalware found!!!\033[00m" "File '$FICHIER' file has been mooved to jail !"
+		echo -e "Malware found" "File '$FICHIER' has been mooved to jail" | mail -s "$user" $email
         fi
 	done
 done
