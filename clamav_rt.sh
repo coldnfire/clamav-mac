f270fd3b3b5a64d899b2eee32404355c31c5f6bc
coldnfire <laboserver@gmail.com>
2019-01-31 15:47:36 +0100

    Global modification

--- clamav_rt.sh
+++ clamav_rt.sh
@@ -22,8 +22,8 @@ fswatch -l 1 $folder |
 while read file; do
 	clamdscan -m -v --fdpass "$file" --move=$jail
         if [ "$?" == "1" ]; then
-		echo -e "Malware found!!!" "File '$file' file has been mooved to jail !" >> $logfile
-		echo -e "Malware found" "File '$file' has been mooved to jail, the user is $user with mac address $mac the result of the scan is in $logfile" | mail -s "$user $mac" $email
+		echo -e "Malware found!!!" "File '$file' file has been moved to jail !" >> $logfile
+		echo -e "Malware found" "File '$file' has been moved to jail, the user is $user with mac address $mac the result of the scan is in $logfile" | mail -s "$user $mac" $email
         fi
 	done
 done
